d82562bf057d7ad5ab39708db1ef2876b48ba650
Snake <chaldrich24@gmail.com>
2021-09-27 21:12:54 -0400

    fix potential errors

--- assets/js/single.js
+++ assets/js/single.js
@@ -5,8 +5,15 @@ var repoNameEl = document.querySelector("#repo-name");
 var getRepoName = function() {
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
-    getRepoIssues(repoName);
-    repoNameEl.textContent = repoName;
+    if(repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    }
+    else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
 };
 
 var getRepoIssues = function(repo) {
@@ -24,7 +31,8 @@ var getRepoIssues = function(repo) {
             });
         }
         else {
-            alert("There was a problem with your request!");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
